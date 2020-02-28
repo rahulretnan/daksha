5e6b315a16b7768fbe92224b09cf955aeb211f5b
rahulagnii <rahul.r4517@gmail.com>
2020-02-28 11:07:41 +0530

    update

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -225,8 +225,8 @@
     // :: 13.0 Countdown Active Code
     // ****************************
     if ($.fn.countdown) {
-        $("#clock").countdown("2020/03/22", function(event) {
-            $(this).html(event.strftime("<div>%m <span>Months</span></div> <div>%d <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
+        $("#clock").countdown('03/22/2020 10:00:00', function(event) {
+            $(this).html(event.strftime("<div>%m <span>Months</span></div> <div>%-D <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
         });
     }
 
